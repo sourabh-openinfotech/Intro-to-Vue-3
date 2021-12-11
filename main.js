02089c0a6ddc28f3635f3c781485c8fb8135eabb
sourabh-openinfotech <sourabh.openinfotech@gmail.com>
2021-12-11 23:18:30 +0530

    added emit from child component and passed data to parent component

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: false,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    removeById(id) {
+      const index = this.cart.indexOf(id);
+      if (index > -1) {
+        this.cart.splice(index, 1);
+        console.log(this.cart);
+      }
+    },
+  },
+  computed: {},
+});
